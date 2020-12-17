5ceb5b10fdc6f092ede82c150949a2b4ad9e7227
Tushe <tushe@mg-comm.com>
2020-12-17 06:08:48 +0200

    init events

---
+++ src/pages/dashboard/Events/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styled, {withTheme} from 'styled-components';
+import {Helmet} from "react-helmet/es/Helmet";
+
+const Events = () => {
+  return (
+    <>
+      <Helmet title="Events"/>
+    </>
+  );
+}
+
+export default withTheme(Events);
