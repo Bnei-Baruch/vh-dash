d8f905f1ad9a40543b2b411f94a0fd0f1f905d08
Yash Shah <yashshh123@gmail.com>
2022-03-17 01:44:34 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ import EventIcon from '@material-ui/icons/Event';
 import HelpIcon from '@material-ui/icons/Help';
 import async from "../components/Async";
 import { DASHBOARD_ROUTES } from "./dashboardRoutes";
-import { ARCHIVE_LINK, BB_HELP_LINK } from "../constants/common";
+import { ARCHIVE_LINK, ARVUT_SYSTEM_URL, BB_HELP_LINK } from "../constants/common";
 
 // Home
 const Home = async(() => import("../pages/dashboard/Home"));
@@ -96,7 +96,7 @@ const dashboardRoutes = [
     children: null,
   },
   {
-    path: DASHBOARD_ROUTES.arvutSystem,
+    path: ARVUT_SYSTEM_URL,
     id: "ArvutSystem",
     icon: <ViewComfyIcon />,
     enableHeader: true,
@@ -104,7 +104,8 @@ const dashboardRoutes = [
       { name: "BroadcastArea", path: DASHBOARD_ROUTES.arvutSystem },
     ],
     component: ArvutSystem,
-    children: null
+    children: null,
+    isExternalLink: true,
   },
   {
     path: ARCHIVE_LINK,
