471146bbc807ad54270021f7824231f55f45ff92
Yash Shah <yashshh123@gmail.com>
2022-05-02 09:02:02 +0000

    Removal of the Events tile from main dashboard

--- src/pages/dashboard/Home/index.js
+++ src/pages/dashboard/Home/index.js
@@ -20,9 +20,9 @@ const Home = () => {
       <Grid container spacing={6}>
         <Grid item xs={12} lg={6}>
           <Grid container spacing={2}>
-             <Grid item xs={12} lg={12}>
+             {/* <Grid item xs={12} lg={12}>
               <Convention liveEvent={liveEvent} />
-            </Grid>
+            </Grid> */}
             <Grid item xs={12} lg={12}>
               <Arvut liveEvent={liveEvent} />
             </Grid>
