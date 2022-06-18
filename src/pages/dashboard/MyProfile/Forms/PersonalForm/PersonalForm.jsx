bd887616a5b4ea117cfcde82c3f660b9d1ec3e22
Yash Shah <yashshh123@gmail.com>
2022-06-19 01:35:17 +0530

    Update PersonalForm.jsx

--- src/pages/dashboard/MyProfile/Forms/PersonalForm/PersonalForm.jsx
+++ src/pages/dashboard/MyProfile/Forms/PersonalForm/PersonalForm.jsx
@@ -16,7 +16,20 @@ import {
   genderData,
   maritalStatuses,
 } from "../../../../../constants/formData";
-
+var month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const PersonalForm = ({ inputFields, handleChange, isModified }) => {
   const classes = commonFormStyles();
   const { t } = useTranslation();
@@ -28,6 +41,22 @@ const PersonalForm = ({ inputFields, handleChange, isModified }) => {
     gender,
     marital_status,
   } = inputFields;
+
+  const getDateValue = (date, type) => {
+    if (date === undefined || date === null) {
+      return undefined;
+    }
+    if (type === 'date' && date) {
+      console.log(date.split('-').pop())
+      return date.split('-').pop()
+    } else if (type == "month" && date) {
+      console.log([date.split('-')[1]])
+      return month[parseInt(date.split('-')[1])]
+    } else {
+      console.log(date.split('-')[0])
+      return date.split('-')[0]
+    }
+  }
 
   return (
     <div className={classes.root}>
@@ -79,35 +108,36 @@ const PersonalForm = ({ inputFields, handleChange, isModified }) => {
             <Select
               labelId="demo-simple-select-outlined-label"
               id="demo-simple-select-outlined"
-              value={date_of_birth}
+              value={date_of_birth ? getDateValue(date_of_birth, 'date') : 1}
               onChange={(e) => handleChange("date_of_birth", e.target.value)}
               label="Age"
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              {[...Array(31 - 1 + 1).keys()]
+                .map((i) => i + 1)
+                .map((i) => (
+                  <MenuItem value={i}>{i}</MenuItem>
+                ))}
             </Select>
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={2}>
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-outlined-label">Month</InputLabel>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Month
+            </InputLabel>
             <Select
               labelId="demo-simple-select-outlined-label"
               id="demo-simple-select-outlined"
-              value={date_of_birth}
+              value={date_of_birth ? getDateValue(date_of_birth, 'month') : 'January'}
               onChange={(e) => handleChange("date_of_birth", e.target.value)}
               label="Age"
             >
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              {month.map((i) => (
+                <MenuItem value={i}>{i}</MenuItem>
+              ))}
             </Select>
           </FormControl>
         </Grid>
@@ -117,16 +147,15 @@ const PersonalForm = ({ inputFields, handleChange, isModified }) => {
             <Select
               labelId="demo-simple-select-outlined-label"
               id="demo-simple-select-outlined"
-              value={date_of_birth}
+              value={date_of_birth ? getDateValue(date_of_birth, 'year') : 1990}
               onChange={(e) => handleChange("date_of_birth", e.target.value)}
               label="Age"
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              {[...Array(2022 - 1990 + 1).keys()]
+                .map((i) => i + 1990)
+                .map((i) => (
+                  <MenuItem value={i}>{i}</MenuItem>
+                ))}
             </Select>
           </FormControl>
         </Grid>
