13ceaf6d7cea7f23e11428e19e7c54e642eae2e0
Yash Shah <yashshh123@gmail.com>
2022-07-29 21:38:19 +0530

    Widget and correct URL for the Convention

--- src/pages/dashboard/Events/Events.js
+++ src/pages/dashboard/Events/Events.js
@@ -480,8 +480,8 @@ export default function Events() {
                           navigate(
                             true,
                             event.url
-                              ? i18n.langauge !== "en" ? event.url + i18n.langauge : event.url
-                              : i18n.langauge !== "en" ? "https://convention.kli.one" + "/" + i18n.langauge : "https://convention.kli.one",
+                              ? i18n.langauge !== "en" ? `${event.url}${i18n.langauge}` : event.url
+                              : i18n.langauge !== "en" ? "https://convention.kli.one/" + i18n.langauge : "https://convention.kli.one",
                             true
                           )
                         }
