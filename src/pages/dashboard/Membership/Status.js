6e2ddebacf4c66cf47f9b22712846b7fbd0fd1a1
Yash Shah <yashshh123@gmail.com>
2023-02-09 12:42:38 +0530

    Updates on membership

--- src/pages/dashboard/Membership/Status.js
+++ src/pages/dashboard/Membership/Status.js
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Card,
   CardContent,
   CardHeader,
@@ -9,14 +10,11 @@ import {
 } from "@material-ui/core";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import PersonOutlinedIcon from "@material-ui/icons/PersonOutlined";
-import EmailOutlinedIcon from "@material-ui/icons/EmailOutlined";
-import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import SmartphoneOutlinedIcon from "@material-ui/icons/SmartphoneOutlined";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import CancelIcon from "@material-ui/icons/Cancel";
 import { useSelector } from "react-redux";
-import { profileInfo } from "../../../redux/selectors/profile";
-import countries from "../../../constants/countries";
-import { firstUpperCase } from "../../../utils";
+import { membershipData } from "../../../redux/selectors/user";
 const useStyles = makeStyles({
   statusContainer: {
     display: "flex",
@@ -33,139 +31,145 @@ const useStyles = makeStyles({
   iconInActive: {
     color: "#ff0000",
   },
-  flexCentered: {
-    display: "flex",
-    alignItems: "center",
-  },
   iconNew: {
     color: "#1976d2",
   },
   greyText: {
     color: "#747474",
-    marginBottom: "10px",
   },
   ctaContainer: {
     textAlign: "right",
   },
 });
-export default function Status({ membershipDetails }) {
+export default function Status() {
   const [status, setStatus] = React.useState("inactive");
   const classes = useStyles();
-  const { t } = useTranslation();
-  const profileData = useSelector(profileInfo);
-
-  const getCountry = (code) => {
-    const country = countries.find((country) => country.ISO === code);
-    return country.label;
+  const { t, i18n } = useTranslation();
+  const membership = useSelector(membershipData);
+  const redirectToPayment = () => {
+    window
+      .open(
+        `${window.location.origin}/pay/membership?lang=${i18n.language}`,
+        "_blank"
+      )
+      .focus();
   };
+
+  // const redirectToPayment = () => {
+  //   window
+  //     .open(
+  //       `${window.location.origin}/pay/membership?lang=${i18n.language}`,
+  //       "_blank"
+  //     )
+  //     .focus();
+  // };
+
   React.useEffect(() => {
-    if (membershipDetails && membershipDetails.active) {
+    if (membership && membership.membership) {
       setStatus("active");
     }
-  }, [membershipDetails]);
+  }, [membership]);
   return (
     <Grid container spacing={6}>
-      <Grid item xs={12}>
-        <CardHeader
-          title={t("Membership.name") + " " + t("Membership.information")}
-        />
-      </Grid>
-      <Grid container item xs={12} lg={12}>
-        <Grid item xs={12} lg={status !== "inactive" ? 2 : 4}>
-          <Card mb={6}>
-            <CardContent>
-              <Box>
-                <Grid className={classes.statusContainer}>
-                  {status === "active" && (
-                    <div>
-                      <Typography variant="h6" className={classes.greyText}>
-                        {t("common.status")}
-                      </Typography>
-                      <Typography variant="h6" className={classes.iconActive}>
-                        {t("Membership.active")}
-                      </Typography>
-                    </div>
-                  )}
-                  {status === "inactive" && (
-                    <div>
-                      <Typography variant="h6" className={classes.greyText}>
-                        {t("common.status")}
-                      </Typography>
-                      <Typography variant="h6">
-                        {t("Membership.inactive")}
-                      </Typography>
-                    </div>
-                  )}
-                </Grid>
-              </Box>
-            </CardContent>
-          </Card>
-        </Grid>
-        {status !== "inactive" && (
-          <Grid item xs={12} lg={2}>
+      <Grid item xs={12} lg={8}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={12}>
             <Card mb={6}>
+              <CardHeader title={t("Membership.name")} />
               <CardContent>
                 <Box>
                   <Grid className={classes.statusContainer}>
-                    <div>
-                      <Typography variant="h6" className={classes.greyText}>
-                        {t("common.type")}
-                      </Typography>
-                      <Typography variant="h6">
-                        {firstUpperCase(membershipDetails.type)}
-                      </Typography>
-                    </div>
+                    {status === "new" && (
+                      <>
+                        <AddBoxIcon
+                          className={[classes.iconStyle, classes.iconNew]}
+                        />
+                        <div>
+                          <Typography variant="h5" className={classes.greyText}>
+                            {t("Membership.myMembership")}
+                          </Typography>
+                          <Typography variant="h4">
+                            {t("Membership.new")}
+                          </Typography>
+                        </div>
+                      </>
+                    )}
+                    {status === "active" && (
+                      <>
+                        <VerifiedUserIcon
+                          className={[classes.iconStyle, classes.iconActive]}
+                        />
+                        <div>
+                          <Typography variant="h5" className={classes.greyText}>
+                            {t("Membership.myMembership")}
+                          </Typography>
+                          <Typography
+                            variant="h4"
+                            className={classes.iconActive}
+                          >
+                            {t("Membership.active")}
+                          </Typography>
+                        </div>
+                      </>
+                    )}
+                    {status === "inactive" && (
+                      <>
+                        <CancelIcon
+                          className={[classes.iconStyle, classes.iconInActive]}
+                        />
+                        <div>
+                          <Typography variant="h5" className={classes.greyText}>
+                            {t("Membership.myMembership")}
+                          </Typography>
+                          <Typography
+                            variant="h4"
+                            className={classes.iconInActive}
+                          >
+                            {t("Membership.inactive")}
+                          </Typography>
+                        </div>
+                      </>
+                    )}
+                  </Grid>
+                  <br />
+                  <Grid>
+                    <Typography variant="h5">
+                      {t("Membership.statusDescription")} :
+                    </Typography>
+                    <Typography variant="body1">
+                      {t(`Membership.${membership.status_name}`)}
+                    </Typography>
+                  </Grid>
+                  <br />
+                  <Grid className={classes.ctaContainer}>
+                    <Button
+                      onClick={redirectToPayment}
+                      variant="contained"
+                      color="primary"
+                    >
+                      {t("UserMenu.payUserFee")}
+                    </Button>
                   </Grid>
                 </Box>
               </CardContent>
             </Card>
           </Grid>
-        )}
-        <Grid item xs={12} lg={3}>
-          <Card mb={6}>
-            <CardContent>
-              <Box>
-                <Grid className={classes.statusContainer}>
-                  <div>
-                    <Typography variant="h6" className={classes.flexCentered}>
-                      <PersonOutlinedIcon /> &nbsp;
-                      {profileData
-                        ? profileData.first_name_vernacular +
-                          " " +
-                          profileData.last_name_vernacular
-                        : ""}
-                    </Typography>
-                    <Typography variant="h6" className={classes.flexCentered}>
-                      <EmailOutlinedIcon /> &nbsp;{" "}
-                      {profileData ? profileData.primary_email : ""}
-                    </Typography>
-                  </div>
-                </Grid>
-              </Box>
-            </CardContent>
-          </Card>
         </Grid>
-        <Grid item xs={12} lg={3}>
-          <Card mb={6}>
-            <CardContent>
-              <Box>
-                <Grid className={classes.statusContainer}>
-                  <div>
-                    <Typography variant="h6" className={classes.flexCentered}>
-                      <LocationOnOutlinedIcon /> &nbsp;{" "}
-                      {profileData && profileData.country
-                        ? getCountry(profileData.country)
-                        : ""}
-                    </Typography>
-                    <Typography variant="h6" className={classes.flexCentered}>
-                      <SmartphoneOutlinedIcon /> &nbsp;{" "}
-                      {profileData ? profileData.mobile_number : ""}
-                    </Typography>
-                  </div>
-                </Grid>
-              </Box>
-            </CardContent>
-          </Card>
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={12}>
+            <Card mb={6}>
+              <CardHeader title={t("Membership.info")} />
+              <CardContent>
+                <Box display="flex" justifyContent="space-between">
+                  <Typography variant="body1">
+                    {t("Membership.membershipDescription")}
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
