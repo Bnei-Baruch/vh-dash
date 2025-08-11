e58dcd344c5a33a6c6ce511f3b93c7a31d73a98a
Edo Shor <edoshor@gmail.com>
2025-08-11 09:47:04 +0300

    bring back membership validation in broadcast

--- src/pages/dashboard/BroadcastArea/BroadcastArea.jsx
+++ src/pages/dashboard/BroadcastArea/BroadcastArea.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Helmet from "react-helmet";
 
 import Broadcast from "./Broadcast";
 import BroadcastQuestions from "./BroadcastQuestions";
-// import MembershipRequired from "./MembershipRequired";
-// import { membershipDataV2 } from "../../../redux/selectors/user";
+import MembershipRequired from "./MembershipRequired";
+import { membershipDataV2 } from "../../../redux/selectors/user";
 
 const useStyles = makeStyles((theme) => ({
   rightButton: {
@@ -20,11 +20,11 @@ const BroadcastArea = () => {
   const classes = useStyles();
   const { t } = useTranslation();
 
-  // const membershipData = useSelector(membershipDataV2);
+  const membershipData = useSelector(membershipDataV2);
 
-  // if (!membershipData?.active) {
-  //   return <MembershipRequired />;
-  // }
+  if (!membershipData?.active) {
+    return <MembershipRequired />;
+  }
 
   return (
     <>
