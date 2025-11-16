a3d19aae7ed820d18f7897c4022ac501f9c841a4
Hila Shor <hilashor@gmail.com>
2025-11-16 13:06:45 +0000

    Feature/add study materials

--- src/pages/dashboard/BroadcastArea/BroadcastArea.jsx
+++ src/pages/dashboard/BroadcastArea/BroadcastArea.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
@@ -10,14 +9,7 @@ import BroadcastQuestions from "./BroadcastQuestions";
 import MembershipRequired from "./MembershipRequired";
 import { membershipDataV2 } from "../../../redux/selectors/user";
 
-const useStyles = makeStyles((theme) => ({
-  rightButton: {
-    marginTop: "10px",
-  },
-}));
-
 const BroadcastArea = () => {
-  const classes = useStyles();
   const { t } = useTranslation();
 
   const membershipData = useSelector(membershipDataV2);
@@ -30,7 +22,7 @@ const BroadcastArea = () => {
     <>
       <Helmet title={t("Dashboard.BroadcastArea.name")} />
       <Grid container spacing={10}>
-        <Grid item xs={12} sm={12} md={12} className={classes.rightButton}>
+        <Grid item xs={12} sm={12} md={12}>
           <Broadcast />
           <BroadcastQuestions />
         </Grid>
