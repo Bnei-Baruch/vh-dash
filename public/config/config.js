b0b3756e4c5b7f71cfddd946b336681a8e162d87
Hila Shor <your@email.com>
2026-03-27 10:31:03 +0300

    fix: add MQTT, MSG and Janus server config to template for production

--- public/config/config.js
+++ public/config/config.js
@@ -8,9 +8,9 @@ window.APP_CONFIG = {
     },
     STUDY_MATERIALS_WIDGET_URL: "https://study.kli.one/widget/widget.js",
     STUDY_MATERIALS_API_URL: "https://study.kli.one/",
-    MQTT_URL: 'msg.kab.info',
-    MSG_URL: 'msg.kab.sh',
-    JANUS_SRV_STR: 'https://str1.kab.sh/janusgxy',
+    MQTT_URL: " ",
+    MSG_URL: " ",
+    JANUS_SRV_STR: " ",
     STRDB_BACKEND: 'https://strdb.kab.sh/',
     STUN_SRV_STR: 'stun:stun1.kab.sh:3478',
     ENABLE_MOCK_MEMBERSHIP: 'false'
