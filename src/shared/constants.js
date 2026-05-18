2ab776285eecde1ac7e643d8580b84af0bbcfe5f
Hila Shor <your@email.com>
2026-05-18 17:06:38 +0300

    Replace home calendar Google API with events API

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,10 +1,5 @@
 export const ARVUT_URL = "https://arvut.kli.one";
-
-export const GOOGLE_CALENDAR_API_KEY = process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY;
-export const GOOGLE_CALENDAR_HE = process.env.REACT_APP_GOOGLE_CALENDAR_ID_HE;
-export const GOOGLE_CALENDAR_RU = process.env.REACT_APP_GOOGLE_CALENDAR_ID_RU;
-export const GOOGLE_CALENDAR_EN = process.env.REACT_APP_GOOGLE_CALENDAR_ID_EN;
-export const GOOGLE_CALENDAR_ES = process.env.REACT_APP_GOOGLE_CALENDAR_ID_ES;
+export const EVENTS_API_URL = "https://events.kli.one/api/events";
 
 // Variables to toggle components
 export const CHAT_AND_NOTIFICATION_ICONS =
